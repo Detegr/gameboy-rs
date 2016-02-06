98bcfc28926dffeaf93ff43c20dec710688f20b6
Antti Keränen <detegr@gmail.com>
2016-02-06 14:09:47 +0200

    Implement DEC, nn

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -72,6 +72,17 @@ macro_rules! ld_r_n {
         $cpu.cycles += 8;
     }
 }
+macro_rules! inc_nn {
+    ($cpu:expr, $r1:ident, $r2:ident) => {{
+        if $cpu.$r2 == 0xFF {
+            $cpu.$r1 = $cpu.$r1.wrapping_add(1);
+            $cpu.$r2 = $cpu.$r2.wrapping_add(1);
+        } else {
+            $cpu.$r2 += 0x1;
+        }
+        $cpu.cycles += 8;
+    }}
+}
 macro_rules! dec_nn {
     ($cpu:expr, $r1:ident, $r2:ident) => {{
         if $cpu.$r2 == 0x0 {
@@ -113,6 +124,13 @@ impl Cpu {
     fn dec_hl(&mut self, _ram: &mut Ram) { dec_nn!(self, h, l) }
     fn dec_sp(&mut self, _ram: &mut Ram) {
         self.sp = self.sp.wrapping_sub(1);
+        self.cycles += 8;
+    }
+    fn inc_bc(&mut self, _ram: &mut Ram) { inc_nn!(self, b, c) }
+    fn inc_de(&mut self, _ram: &mut Ram) { inc_nn!(self, d, e) }
+    fn inc_hl(&mut self, _ram: &mut Ram) { inc_nn!(self, h, l) }
+    fn inc_sp(&mut self, _ram: &mut Ram) {
+        self.sp = self.sp.wrapping_add(1);
         self.cycles += 8;
     }
 
@@ -312,6 +330,35 @@ mod test {
     }
 
     #[test]
+    fn test_inc_nn() {
+        macro_rules! test_inc_nn(
+            ($r1:ident, $r2:ident, $func:expr) => {{
+                let (mut cpu, mut ram) = init(None);
+                cpu.$r1 = 0x0;
+                cpu.$r2 = 0x0;
+                test(&mut cpu, &mut ram, 8, $func);
+                assert!(cpu.$r1 == 0x00, format!("inc {}{}: Expected {}, got {}", stringify!($r1), stringify!($r2), 0x00, cpu.$r1));
+                assert!(cpu.$r2 == 0x01, format!("inc {}{}: Expected {}, got {}", stringify!($r1), stringify!($r2), 0x01, cpu.$r2));
+
+                cpu.$r1 = 0x2;
+                cpu.$r2 = 0xFF;
+                test(&mut cpu, &mut ram, 8, $func);
+                assert!(cpu.$r1 == 0x3, format!("inc {}{}: Expected {}, got {}", stringify!($r1), stringify!($r2), 0x3, cpu.$r1));
+                assert!(cpu.$r2 == 0x0, format!("inc {}{}: Expected {}, got {}", stringify!($r1), stringify!($r2), 0x0, cpu.$r2));
+
+                cpu.$r1 = 0xFF;
+                cpu.$r2 = 0xFF;
+                test(&mut cpu, &mut ram, 8, $func);
+                assert!(cpu.$r1 == 0x0, format!("inc {}{}: Expected {}, got {}", stringify!($r1), stringify!($r2), 0x0, cpu.$r1));
+                assert!(cpu.$r2 == 0x0, format!("inc {}{}: Expected {}, got {}", stringify!($r1), stringify!($r2), 0x0, cpu.$r2));
+            }}
+        );
+        test_inc_nn!(b, c, opcode(0x03));
+        test_inc_nn!(d, e, opcode(0x13));
+        test_inc_nn!(h, l, opcode(0x23));
+    }
+
+    #[test]
     fn test_dec_sp() {
         let (mut cpu, mut ram) = init(None);
         cpu.sp = 1;
@@ -321,6 +368,18 @@ mod test {
         assert!(cpu.sp == 0xFFFF);
         test(&mut cpu, &mut ram, 8, opcode(0x3B));
         assert!(cpu.sp == 0xFFFE);
+    }
+
+    #[test]
+    fn test_inc_sp() {
+        let (mut cpu, mut ram) = init(None);
+        cpu.sp = 0xFFFE;
+        test(&mut cpu, &mut ram, 8, opcode(0x33));
+        assert!(cpu.sp == 0xFFFF);
+        test(&mut cpu, &mut ram, 8, opcode(0x33));
+        assert!(cpu.sp == 0x0);
+        test(&mut cpu, &mut ram, 8, opcode(0x33));
+        assert!(cpu.sp == 0x1);
     }
 
     #[test]
