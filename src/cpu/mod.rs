17cab150c7232bccc5a85bafedd80d94b927a384
Antti Keränen <detegr@gmail.com>
2018-09-16 14:40:15 +0300

    Define most of the member functions within macros

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -37,36 +37,57 @@ impl Cpu {
         (self.h as u16) << 8 | self.l as u16
     }
 }
-macro_rules! ld_rr_nn {
-    ($cpu:expr, $ram:expr, $n1:ident, $n2:ident) => {
-        $cpu.$n2 = $cpu.next_byte($ram);
-        $cpu.$n1 = $cpu.next_byte($ram);
-        $cpu.cycles += 12;
-    };
+macro_rules! make_ld_rr_nn {
+    ($name: ident, $r1:ident, $r2:ident) => {
+        #[inline]
+        fn $name(&mut self, ram: &mut Ram) {
+            self.$r2 = self.next_byte(ram);
+            self.$r1 = self.next_byte(ram);
+            self.cycles += 12;
+        }
+    }
 }
-macro_rules! ld_r1_r2 {
-    ($cpu:expr, $r1:ident, $r2:ident) => {
-        $cpu.$r1 = $cpu.$r2;
-        $cpu.cycles += 4;
-    };
+macro_rules! make_ld_r_r {
+    ($name:ident, $r1:ident, $r2:ident) => {
+        #[inline]
+        fn $name(&mut self, _ram: &mut Ram) {
+            self.$r1 = self.$r2;
+            self.cycles += 4;
+        }
+    }
 }
-macro_rules! ld_r_rr {
-    ($cpu:expr, $ram:expr, $r:ident, $rr:ident) => {
-        $cpu.$r = $ram[$cpu.$rr() as usize];
-        $cpu.cycles += 8;
-    };
+macro_rules! make_ld_r_rr {
+    ($name: ident, $r:ident, $rr:ident) => {
+        fn $name(&mut self, ram: &mut Ram) {
+            self.$r = ram[self.$rr() as usize];
+            self.cycles += 8;
+        }
+    }
 }
-macro_rules! ld_rr_r {
-    ($cpu:expr, $ram:expr, $rr:ident, $r:ident) => {
-        $ram[$cpu.$rr() as usize] = $cpu.$r;
-        $cpu.cycles += 8;
-    };
+macro_rules! make_ld_rr_r {
+    ($name: ident, $rr:ident, $r:ident) => {
+        fn $name(&mut self, ram: &mut Ram) {
+            ram[self.$rr() as usize] = self.$r;
+            self.cycles += 8;
+        }
+    }
 }
-macro_rules! ld_r_n {
-    ($cpu:expr, $ram:expr, $r:ident) => {
-        $cpu.$r = $cpu.next_byte($ram);
-        $cpu.cycles += 8;
-    };
+macro_rules! make_ld_r_n {
+    ($name: ident, $r:ident) => {
+        #[inline]
+        fn $name(&mut self, ram: &mut Ram) {
+            self.$r = self.next_byte(ram);
+            self.cycles += 8;
+        }
+    }
+}
+macro_rules! make_add {
+    ($name:ident, $reg: ident) => {
+        #[inline]
+        fn $name(&mut self, _ram: &mut Ram) {
+            add_a_r!(self, self.$reg);
+        }
+    }
 }
 macro_rules! add_a_r {
     ($cpu:expr, $value:expr) => {{
@@ -87,16 +108,27 @@ macro_rules! add_a_r {
         $cpu.cycles += 4;
     }};
 }
-macro_rules! inc_rr {
-    ($cpu:expr, $r1:ident, $r2:ident) => {{
-        if $cpu.$r2 == 0xFF {
-            $cpu.$r1 = $cpu.$r1.wrapping_add(1);
-            $cpu.$r2 = $cpu.$r2.wrapping_add(1);
-        } else {
-            $cpu.$r2 += 0x1;
+macro_rules! make_inc_rr {
+    ($name:ident, $r1:ident, $r2:ident) => {
+        #[inline]
+        fn $name(&mut self, _ram: &mut Ram) {
+            if self.$r2 == 0xFF {
+                self.$r1 = self.$r1.wrapping_add(1);
+                self.$r2 = self.$r2.wrapping_add(1);
+            } else {
+                self.$r2 += 0x1;
+            }
+            self.cycles += 8;
         }
-        $cpu.cycles += 8;
-    }};
+    }
+}
+macro_rules! make_inc {
+    ($name:ident, $reg: ident) => {
+        #[inline]
+        fn $name(&mut self, _ram: &mut Ram) {
+            self.$reg = inc_r!(self, self.$reg);
+        }
+    }
 }
 macro_rules! inc_r {
     ($cpu:expr, $value:expr) => {{
@@ -113,6 +145,14 @@ macro_rules! inc_r {
         $value
     }};
 }
+macro_rules! make_dec {
+    ($name:ident, $reg: ident) => {
+        #[inline]
+        fn $name(&mut self, _ram: &mut Ram) {
+            self.$reg = dec_r!(self, self.$reg);
+        }
+    }
+}
 macro_rules! dec_r {
     ($cpu:expr, $value:expr) => {{
         let check = $value;
@@ -128,26 +168,31 @@ macro_rules! dec_r {
         $value
     }};
 }
-macro_rules! dec_rr {
-    ($cpu:expr, $r1:ident, $r2:ident) => {{
-        if $cpu.$r2 == 0x0 {
-            $cpu.$r1 = $cpu.$r1.wrapping_sub(1);
-            $cpu.$r2 = $cpu.$r2.wrapping_sub(1);
-        } else {
-            $cpu.$r2 -= 0x1;
+macro_rules! make_dec_rr {
+    ($name:ident, $r1:ident, $r2:ident) => {
+        #[inline]
+        fn $name(&mut self, _ram: &mut Ram) {
+            if self.$r2 == 0x0 {
+                self.$r1 = self.$r1.wrapping_sub(1);
+                self.$r2 = self.$r2.wrapping_sub(1);
+            } else {
+                self.$r2 -= 0x1;
+            }
+            self.cycles += 8;
         }
-        $cpu.cycles += 8;
-    }};
+    }
 }
 
 impl Cpu {
     pub fn new() -> Cpu {
         Cpu::default()
     }
+    #[inline]
     pub fn _reset(&mut self) {
         self.sp = 0xFFFE;
         self.pc = 0x100;
     }
+    #[inline]
     fn next_byte(&mut self, ram: &mut Ram) -> u8 {
         if self.pc > 65536 {
             panic!("Program counter out of bounds")
@@ -157,130 +202,80 @@ impl Cpu {
         ret
     }
 
+    #[inline]
     fn nyi(&mut self, _: &mut Ram) {
         panic!("Instruction not yet implemented")
     }
+
+    #[inline]
     fn nop(&mut self, _: &mut Ram) {
         self.cycles += 8;
     }
 
-    fn add_a_a(&mut self, _ram: &mut Ram) {
-        add_a_r!(self, self.a);
-    }
-    fn add_a_b(&mut self, _ram: &mut Ram) {
-        add_a_r!(self, self.b);
-    }
-    fn add_a_c(&mut self, _ram: &mut Ram) {
-        add_a_r!(self, self.c);
-    }
-    fn add_a_d(&mut self, _ram: &mut Ram) {
-        add_a_r!(self, self.d);
-    }
-    fn add_a_e(&mut self, _ram: &mut Ram) {
-        add_a_r!(self, self.e);
-    }
-    fn add_a_h(&mut self, _ram: &mut Ram) {
-        add_a_r!(self, self.h);
-    }
-    fn add_a_l(&mut self, _ram: &mut Ram) {
-        add_a_r!(self, self.l);
-    }
+    make_add!(add_a_a, a);
+    make_add!(add_a_b, b);
+    make_add!(add_a_c, c);
+    make_add!(add_a_d, d);
+    make_add!(add_a_e, e);
+    make_add!(add_a_h, h);
+    make_add!(add_a_l, l);
+
+    #[inline]
     fn add_a_hl(&mut self, ram: &mut Ram) {
         add_a_r!(self, ram[self.hl() as usize]);
         self.cycles += 4;
     }
 
-    fn inc_a(&mut self, _ram: &mut Ram) {
-        self.a = inc_r!(self, self.a);
-    }
-    fn inc_b(&mut self, _ram: &mut Ram) {
-        self.b = inc_r!(self, self.b);
-    }
-    fn inc_c(&mut self, _ram: &mut Ram) {
-        self.c = inc_r!(self, self.c);
-    }
-    fn inc_d(&mut self, _ram: &mut Ram) {
-        self.d = inc_r!(self, self.d);
-    }
-    fn inc_e(&mut self, _ram: &mut Ram) {
-        self.e = inc_r!(self, self.e);
-    }
-    fn inc_h(&mut self, _ram: &mut Ram) {
-        self.h = inc_r!(self, self.h);
-    }
-    fn inc_l(&mut self, _ram: &mut Ram) {
-        self.l = inc_r!(self, self.l);
-    }
-    fn inc_hl(&mut self, ram: &mut Ram) {
+    make_inc!(inc_a, a);
+    make_inc!(inc_b, b);
+    make_inc!(inc_c, c);
+    make_inc!(inc_d, d);
+    make_inc!(inc_e, e);
+    make_inc!(inc_h, h);
+    make_inc!(inc_l, l);
+
+    #[inline]
+    fn inc_deref_hl(&mut self, ram: &mut Ram) {
         ram[self.hl() as usize] = inc_r!(self, ram[self.hl() as usize]);
         self.cycles += 8;
     }
 
-    fn dec_a(&mut self, _ram: &mut Ram) {
-        self.a = dec_r!(self, self.a);
-    }
-    fn dec_b(&mut self, _ram: &mut Ram) {
-        self.b = dec_r!(self, self.b);
-    }
-    fn dec_c(&mut self, _ram: &mut Ram) {
-        self.c = dec_r!(self, self.c);
-    }
-    fn dec_d(&mut self, _ram: &mut Ram) {
-        self.d = dec_r!(self, self.d);
-    }
-    fn dec_e(&mut self, _ram: &mut Ram) {
-        self.e = dec_r!(self, self.e);
-    }
-    fn dec_h(&mut self, _ram: &mut Ram) {
-        self.h = dec_r!(self, self.h);
-    }
-    fn dec_l(&mut self, _ram: &mut Ram) {
-        self.l = dec_r!(self, self.l);
-    }
-    fn dec_hl(&mut self, ram: &mut Ram) {
+    make_dec!(dec_a, a);
+    make_dec!(dec_b, b);
+    make_dec!(dec_c, c);
+    make_dec!(dec_d, d);
+    make_dec!(dec_e, e);
+    make_dec!(dec_h, h);
+    make_dec!(dec_l, l);
+
+    #[inline]
+    fn dec_deref_hl(&mut self, ram: &mut Ram) {
         ram[self.hl() as usize] = dec_r!(self, ram[self.hl() as usize]);
         self.cycles += 8;
     }
 
-    fn dec_combined_bc(&mut self, _ram: &mut Ram) {
-        dec_rr!(self, b, c)
-    }
-    fn dec_combined_de(&mut self, _ram: &mut Ram) {
-        dec_rr!(self, d, e)
-    }
-    fn dec_combined_hl(&mut self, _ram: &mut Ram) {
-        dec_rr!(self, h, l)
-    }
-    fn dec_combined_sp(&mut self, _ram: &mut Ram) {
+    make_dec_rr!(dec_bc, b, c);
+    make_dec_rr!(dec_de, d, e);
+    make_dec_rr!(dec_hl, h, l);
+    #[inline]
+    fn dec_sp(&mut self, _ram: &mut Ram) {
         self.sp = self.sp.wrapping_sub(1);
         self.cycles += 8;
     }
-    fn inc_combined_bc(&mut self, _ram: &mut Ram) {
-        inc_rr!(self, b, c)
-    }
-    fn inc_combined_de(&mut self, _ram: &mut Ram) {
-        inc_rr!(self, d, e)
-    }
-    fn inc_combined_hl(&mut self, _ram: &mut Ram) {
-        inc_rr!(self, h, l)
-    }
-    fn inc_combined_sp(&mut self, _ram: &mut Ram) {
+    make_inc_rr!(inc_bc, b, c);
+    make_inc_rr!(inc_de, d, e);
+    make_inc_rr!(inc_hl, h, l);
+    #[inline]
+    fn inc_sp(&mut self, _ram: &mut Ram) {
         self.sp = self.sp.wrapping_add(1);
         self.cycles += 8;
     }
 
-    fn ld_bc_a(&mut self, ram: &mut Ram) {
-        ld_rr_r!(self, ram, bc, a);
-    }
-    fn ld_bc_nn(&mut self, ram: &mut Ram) {
-        ld_rr_nn!(self, ram, b, c);
-    }
-    fn ld_de_a(&mut self, ram: &mut Ram) {
-        ld_rr_r!(self, ram, de, a);
-    }
-    fn ld_de_nn(&mut self, ram: &mut Ram) {
-        ld_rr_nn!(self, ram, d, e);
-    }
+    make_ld_rr_r!(ld_bc_a, bc, a);
+    make_ld_rr_nn!(ld_bc_nn, b, c);
+    make_ld_rr_r!(ld_de_a, de, a);
+    make_ld_rr_nn!(ld_de_nn, d, e);
+    #[inline]
     fn ld_nn_a(&mut self, ram: &mut Ram) {
         let mut addr = 0u16;
         addr |= self.next_byte(ram) as u16;
@@ -288,47 +283,27 @@ impl Cpu {
         ram[addr as usize] = self.a;
         self.cycles += 16;
     }
-    fn ld_hl_nn(&mut self, ram: &mut Ram) {
-        ld_rr_nn!(self, ram, h, l);
-    }
+    make_ld_rr_nn!(ld_hl_nn, h, l);
+    #[inline]
     fn ld_sp_nn(&mut self, ram: &mut Ram) {
         self.sp = LittleEndian::read_u16(&ram[self.pc..]);
         self.pc += 2;
         self.cycles += 12;
     }
 
-    fn ld_a_a(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, a, a);
-    }
-    fn ld_a_b(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, a, b);
-    }
-    fn ld_a_c(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, a, c);
-    }
-    fn ld_a_d(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, a, d);
-    }
-    fn ld_a_e(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, a, e);
-    }
-    fn ld_a_h(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, a, h);
-    }
-    fn ld_a_l(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, a, l);
-    }
-    fn ld_a_hl(&mut self, ram: &mut Ram) {
-        ld_r_rr!(self, ram, a, hl);
-    }
-    fn ld_a_bc(&mut self, ram: &mut Ram) {
-        self.a = ram[self.bc() as usize];
-        self.cycles += 8;
-    }
-    fn ld_a_de(&mut self, ram: &mut Ram) {
-        self.a = ram[self.de() as usize];
-        self.cycles += 8;
-    }
+    make_ld_r_r!(ld_a_a, a, a);
+    make_ld_r_r!(ld_a_b, a, b);
+    make_ld_r_r!(ld_a_c, a, c);
+    make_ld_r_r!(ld_a_d, a, d);
+    make_ld_r_r!(ld_a_e, a, e);
+    make_ld_r_r!(ld_a_h, a, h);
+    make_ld_r_r!(ld_a_l, a, l);
+
+    make_ld_r_rr!(ld_a_hl, a, hl);
+    make_ld_r_rr!(ld_a_bc, a, bc);
+    make_ld_r_rr!(ld_a_de, a, de);
+
+    #[inline]
     fn ld_a_nn(&mut self, ram: &mut Ram) {
         let mut addr = 0u16;
         addr |= self.next_byte(ram) as u16;
@@ -336,210 +311,96 @@ impl Cpu {
         self.a = ram[addr as usize];
         self.cycles += 16;
     }
+    #[inline]
     fn ld_a_addr_c(&mut self, ram: &mut Ram) {
         self.a = ram[0xFF00 | (self.c as usize)];
         self.cycles += 8;
     }
+    #[inline]
     fn ld_addr_c_a(&mut self, ram: &mut Ram) {
         ram[0xFF00 | (self.c as usize)] = self.a;
         self.cycles += 8;
     }
 
-    fn ld_b_a(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, b, a);
-    }
-    fn ld_b_b(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, b, b);
-    }
-    fn ld_b_c(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, b, c);
-    }
-    fn ld_b_d(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, b, d);
-    }
-    fn ld_b_e(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, b, e);
-    }
-    fn ld_b_h(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, b, h);
-    }
-    fn ld_b_l(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, b, l);
-    }
-    fn ld_b_hl(&mut self, ram: &mut Ram) {
-        ld_r_rr!(self, ram, b, hl);
-    }
+    make_ld_r_r!(ld_b_a, b, a);
+    make_ld_r_r!(ld_b_b, b, b);
+    make_ld_r_r!(ld_b_c, b, c);
+    make_ld_r_r!(ld_b_d, b, d);
+    make_ld_r_r!(ld_b_e, b, e);
+    make_ld_r_r!(ld_b_h, b, h);
+    make_ld_r_r!(ld_b_l, b, l);
 
-    fn ld_c_a(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, c, a);
-    }
-    fn ld_c_b(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, c, b);
-    }
-    fn ld_c_c(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, c, c);
-    }
-    fn ld_c_d(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, c, d);
-    }
-    fn ld_c_e(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, c, e);
-    }
-    fn ld_c_h(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, c, h);
-    }
-    fn ld_c_l(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, c, l);
-    }
-    fn ld_c_hl(&mut self, ram: &mut Ram) {
-        ld_r_rr!(self, ram, c, hl);
-    }
+    make_ld_r_rr!(ld_b_hl, b, hl);
 
-    fn ld_d_a(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, d, a);
-    }
-    fn ld_d_b(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, d, b);
-    }
-    fn ld_d_c(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, d, c);
-    }
-    fn ld_d_d(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, d, d);
-    }
-    fn ld_d_e(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, d, e);
-    }
-    fn ld_d_h(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, d, h);
-    }
-    fn ld_d_l(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, d, l);
-    }
-    fn ld_d_hl(&mut self, ram: &mut Ram) {
-        ld_r_rr!(self, ram, d, hl);
-    }
+    make_ld_r_r!(ld_c_a, c, a);
+    make_ld_r_r!(ld_c_b, c, b);
+    make_ld_r_r!(ld_c_c, c, c);
+    make_ld_r_r!(ld_c_d, c, d);
+    make_ld_r_r!(ld_c_e, c, e);
+    make_ld_r_r!(ld_c_h, c, h);
+    make_ld_r_r!(ld_c_l, c, l);
 
-    fn ld_e_a(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, e, a);
-    }
-    fn ld_e_b(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, e, b);
-    }
-    fn ld_e_c(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, e, c);
-    }
-    fn ld_e_d(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, e, d);
-    }
-    fn ld_e_e(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, e, e);
-    }
-    fn ld_e_h(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, e, h);
-    }
-    fn ld_e_l(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, e, l);
-    }
-    fn ld_e_hl(&mut self, ram: &mut Ram) {
-        ld_r_rr!(self, ram, e, hl);
-    }
+    make_ld_r_rr!(ld_c_hl, c, hl);
 
-    fn ld_h_a(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, h, a);
-    }
-    fn ld_h_b(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, h, b);
-    }
-    fn ld_h_c(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, h, c);
-    }
-    fn ld_h_d(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, h, d);
-    }
-    fn ld_h_e(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, h, e);
-    }
-    fn ld_h_h(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, h, h);
-    }
-    fn ld_h_l(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, h, l);
-    }
-    fn ld_h_hl(&mut self, ram: &mut Ram) {
-        ld_r_rr!(self, ram, h, hl);
-    }
+    make_ld_r_r!(ld_d_a, d, a);
+    make_ld_r_r!(ld_d_b, d, b);
+    make_ld_r_r!(ld_d_c, d, c);
+    make_ld_r_r!(ld_d_d, d, d);
+    make_ld_r_r!(ld_d_e, d, e);
+    make_ld_r_r!(ld_d_h, d, h);
+    make_ld_r_r!(ld_d_l, d, l);
 
-    fn ld_l_a(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, l, a);
-    }
-    fn ld_l_b(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, l, b);
-    }
-    fn ld_l_c(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, l, c);
-    }
-    fn ld_l_d(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, l, d);
-    }
-    fn ld_l_e(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, l, e);
-    }
-    fn ld_l_h(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, l, h);
-    }
-    fn ld_l_l(&mut self, _ram: &mut Ram) {
-        ld_r1_r2!(self, l, l);
-    }
-    fn ld_l_hl(&mut self, ram: &mut Ram) {
-        ld_r_rr!(self, ram, l, hl);
-    }
+    make_ld_r_rr!(ld_d_hl, d, hl);
 
-    fn ld_hl_a(&mut self, ram: &mut Ram) {
-        ld_rr_r!(self, ram, hl, a);
-    }
-    fn ld_hl_b(&mut self, ram: &mut Ram) {
-        ld_rr_r!(self, ram, hl, b);
-    }
-    fn ld_hl_c(&mut self, ram: &mut Ram) {
-        ld_rr_r!(self, ram, hl, c);
-    }
-    fn ld_hl_d(&mut self, ram: &mut Ram) {
-        ld_rr_r!(self, ram, hl, d);
-    }
-    fn ld_hl_e(&mut self, ram: &mut Ram) {
-        ld_rr_r!(self, ram, hl, e);
-    }
-    fn ld_hl_h(&mut self, ram: &mut Ram) {
-        ld_rr_r!(self, ram, hl, h);
-    }
-    fn ld_hl_l(&mut self, ram: &mut Ram) {
-        ld_rr_r!(self, ram, hl, l);
-    }
+    make_ld_r_r!(ld_e_a, e, a);
+    make_ld_r_r!(ld_e_b, e, b);
+    make_ld_r_r!(ld_e_c, e, c);
+    make_ld_r_r!(ld_e_d, e, d);
+    make_ld_r_r!(ld_e_e, e, e);
+    make_ld_r_r!(ld_e_h, e, h);
+    make_ld_r_r!(ld_e_l, e, l);
+
+    make_ld_r_rr!(ld_e_hl, e, hl);
+
+    make_ld_r_r!(ld_h_a, h, a);
+    make_ld_r_r!(ld_h_b, h, b);
+    make_ld_r_r!(ld_h_c, h, c);
+    make_ld_r_r!(ld_h_d, h, d);
+    make_ld_r_r!(ld_h_e, h, e);
+    make_ld_r_r!(ld_h_h, h, h);
+    make_ld_r_r!(ld_h_l, h, l);
+
+    make_ld_r_rr!(ld_h_hl, h, hl);
+
+    make_ld_r_r!(ld_l_a, l, a);
+    make_ld_r_r!(ld_l_b, l, b);
+    make_ld_r_r!(ld_l_c, l, c);
+    make_ld_r_r!(ld_l_d, l, d);
+    make_ld_r_r!(ld_l_e, l, e);
+    make_ld_r_r!(ld_l_h, l, h);
+    make_ld_r_r!(ld_l_l, l, l);
+
+    make_ld_r_rr!(ld_l_hl, l, hl);
+
+    make_ld_rr_r!(ld_hl_a, hl, a);
+    make_ld_rr_r!(ld_hl_b, hl, b);
+    make_ld_rr_r!(ld_hl_c, hl, c);
+    make_ld_rr_r!(ld_hl_d, hl, d);
+    make_ld_rr_r!(ld_hl_e, hl, e);
+    make_ld_rr_r!(ld_hl_h, hl, h);
+    make_ld_rr_r!(ld_hl_l, hl, l);
+
+    #[inline]
     fn ld_hl_n(&mut self, ram: &mut Ram) {
         ram[self.hl() as usize] = self.next_byte(ram);
         self.cycles += 12;
     }
 
-    fn ld_a_n(&mut self, ram: &mut Ram) {
-        ld_r_n!(self, ram, a);
-    }
-    fn ld_b_n(&mut self, ram: &mut Ram) {
-        ld_r_n!(self, ram, b);
-    }
-    fn ld_c_n(&mut self, ram: &mut Ram) {
-        ld_r_n!(self, ram, c);
-    }
-    fn ld_d_n(&mut self, ram: &mut Ram) {
-        ld_r_n!(self, ram, d);
-    }
-    fn ld_e_n(&mut self, ram: &mut Ram) {
-        ld_r_n!(self, ram, e);
-    }
-    fn ld_h_n(&mut self, ram: &mut Ram) {
-        ld_r_n!(self, ram, h);
-    }
-    fn ld_l_n(&mut self, ram: &mut Ram) {
-        ld_r_n!(self, ram, l);
-    }
+    make_ld_r_n!(ld_a_n, a);
+    make_ld_r_n!(ld_b_n, b);
+    make_ld_r_n!(ld_c_n, c);
+    make_ld_r_n!(ld_d_n, d);
+    make_ld_r_n!(ld_e_n, e);
+    make_ld_r_n!(ld_h_n, h);
+    make_ld_r_n!(ld_l_n, l);
 }
